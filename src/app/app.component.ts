2139d1829b90c40a22acf7091e678312378478b6
Paulo Martins <paulo.martins@vokal.io>
2021-04-20 11:36:27 -0300

    demo-base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Parts', url: '/parts', icon: 'extension-puzzle' },
-    { title: 'Orders', url: '/orders', icon: 'cart' },
-/*  
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-*/  
-  ];
-  constructor() {}
+	public appPages = [
+		{ title: 'HOME (Parts Search)', url: '/parts', icon: 'home' },
+		{ title: 'CUSTOMER BACKLOG', url: '/orders', icon: 'ticket' },
+		{ title: 'APPROVALS', url: '/orders', icon: 'checkmark' },
+	];
+
+	public myAppPages = [
+		{ title: 'FEEDBACK', url: '/parts', icon: 'chatbubble' },
+		{ title: 'WHAT\'S NEW', url: '/orders', icon: 'sparkles' },
+	];
+
+	public darkMode: boolean;
+
+	constructor(
+	) {
+		this.Initialize();
+	}
+
+	private async Initialize() {
+		this.changeLights(true);
+		const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+		prefersDark.addListener((setting) => this.changeLights(setting.matches));
+	}
+
+	private changeLights(dark: boolean){
+		console.info("setting lights to ", dark);
+		this.darkMode = dark;
+		document.body.classList.toggle('dark', dark);
+	}
+	public pressLightSwitch(event) {
+		this.changeLights(event.detail.checked);
+	}
 }
